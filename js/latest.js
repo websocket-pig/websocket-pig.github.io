4f020135ed2dac34c80c18eb303da8cc38c2800c
PIG-cmd <71576224+PIG-cmd@users.noreply.github.com>
2021-02-28 14:27:12 +0800

    Update latest.js

--- js/latest.js
+++ js/latest.js
@@ -4,7 +4,10 @@
 	xhr.open("GET","https://websocket-pig.github.io/js/" + version + ".js",false);
 	xhr.send();
 	if (xhr.status == 200)
+	{
 		eval(xhr.result);
+		
+	}
 	else
 		throw("The latest WebSocket file could not be obtained");
 }

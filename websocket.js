8a1f68ef834a3af2da2cc2a2f1d65935ef0304b2
PIG-cmd <71576224+PIG-cmd@users.noreply.github.com>
2021-02-28 09:54:49 +0800

    Update websocket.js

--- websocket.js
+++ websocket.js
@@ -1,1 +1,16 @@
-
+var websocket = {};
+websocket.init = function(str)
+{
+  if (typeof str == "undefined")
+    str = "latest";
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST","https://websocket-pig.github.io/js/" + str + ".js",false);
+  xhr.send();
+  if (xhr.status == 200)
+  {
+    eval(xhr.responseText);
+    return {"status":200,"result":xhr.responseText};
+  } else {
+    throw("Connot initialization the websocket.");
+  }
+};
